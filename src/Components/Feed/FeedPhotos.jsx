e7cfdc7ac802ec13d3037e9319865c8902783652
felipeSilv4Dev <felipesilva.developer@gmail.com>
2023-12-12 14:36:56 -0300

    adding Feed

--- src/Components/Feed/FeedPhotos.jsx
+++ src/Components/Feed/FeedPhotos.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import FeedPhotosItem from "./FeedPhotosItem";
 import useFetch from "../../Hooks/useFetch";
 import { PHOTOS_GET } from "../../api";
+import Error from "../Helper/Error";
+import Loading from "../Helper/Loading";
+import styles from "./FeedPhotos.module.css";
 
 const FeedPhotos = () => {
   const { data, loading, error, request } = useFetch();
@@ -10,15 +13,23 @@ const FeedPhotos = () => {
     const fetchPhotos = async () => {
       const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
       const { response, json } = await request(url, options);
+
+      console.log(json);
     };
     fetchPhotos();
   }, [request]);
 
-  return (
-    <div>
-      <FeedPhotosItem />
-    </div>
-  );
+  if (error) return <Error error={error} />;
+  if (loading) return <Loading />;
+  if (data)
+    return (
+      <ul className={`animeLeft ${styles.feed}`}>
+        {data.map((photo) => (
+          <FeedPhotosItem key={photo.id} photo={photo} />
+        ))}
+      </ul>
+    );
+  else return null;
 };
 
 export default FeedPhotos;
