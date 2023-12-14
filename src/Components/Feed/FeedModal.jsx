dacf74800367af82750f2dea9f1d7567c5f45f95
felipeSilv4Dev <felipesilva.developer@gmail.com>
2023-12-14 15:01:47 -0300

    adding modal part 2

--- src/Components/Feed/FeedModal.jsx
+++ src/Components/Feed/FeedModal.jsx
@@ -6,16 +6,18 @@ import Error from "../Helper/Error";
 import Loading from "../Helper/Loading";
 import PhotoContent from "../Photo/PhotoContent";
 
-const FeedModal = ({ photo }) => {
+const FeedModal = ({ photo, setModalPhoto }) => {
   const { data, error, loading, request } = useFetch();
 
   React.useEffect(() => {
     const { url, options } = PHOTO_GET(photo.id);
     request(url, options);
   }, [photo, request]);
-
+  const handleOutsideClick = ({ target, currentTarget }) => {
+    if (target === currentTarget) setModalPhoto(null);
+  };
   return (
-    <div className={styles.modal}>
+    <div onClick={handleOutsideClick} className={styles.modal}>
       {error && <Error error={error} />}
       {loading && <Loading />}
       {data && <PhotoContent data={data} />}
